a1c9957563852e5b9b5f8c0f918c5f57e9d6bfc8
HATHECHI <you@example.com>
2022-07-04 10:20:06 +0700

    new

--- app/src/main/java/com/example/android_test/ThongTin.java
+++ app/src/main/java/com/example/android_test/ThongTin.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,22 +15,44 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ThongTin extends AppCompatActivity {
+    EditText Name, Sdt;
+    Button btnThem;
+    ArrayList<InfoStudent> arrayList = new ArrayList<InfoStudent>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thong_tin);
-        ListView list = findViewById(R.id.id_listview);
 
-
-        ArrayList<InfoStudent> arrayList = new ArrayList<InfoStudent>();
         arrayList.add(new InfoStudent("Đại Nghĩa", R.drawable.emone, "0234455677"));
         arrayList.add(new InfoStudent("Trung Quân", R.drawable.emtwo, "0345666777"));
         arrayList.add(new InfoStudent("Hữu Nam", R.drawable.emthree, "0334564666"));
         arrayList.add(new InfoStudent("Văn Việt", R.drawable.emone, "0334564666"));
-        arrayList.add(new InfoStudent("Văn Lâm", R.drawable.emtwo, "0334564666"));
-        JavaCustomListview custom = new JavaCustomListview(this, arrayList);
+
+        ListView list = findViewById(R.id.id_listview);
+
+        JavaCustomListview custom = new JavaCustomListview(ThongTin.this, arrayList);
         list.setAdapter(custom);
+
+        Name = findViewById(R.id.etName_thongtin);
+        Sdt = findViewById(R.id.etSdt_thongtin);
+        btnThem = findViewById(R.id.btnAdd_thongtin);
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = Name.getText().toString();
+                String sdt = Sdt.getText().toString();
+                if (name.isEmpty() || sdt.isEmpty()) {
+                    Toast.makeText(ThongTin.this, "NHẬP ĐỦ DỮ LIỆU!", Toast.LENGTH_SHORT).show();
+                } else {
+                    arrayList.add(new InfoStudent(name, R.drawable.emtwo, sdt));
+
+                    custom.notifyDataSetChanged();
+                }
+            }
+        });
+
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
